4bd8eb0acb72dfb8873635ec9e292c88120c1799
albert vila <albert.vila@inqbarna.com>
2025-05-29 10:34:09 +0200

    added

--- Sources/Database/DatabaseManager.swift
+++ Sources/Database/DatabaseManager.swift
@@ -14,7 +14,7 @@ public final class DatabaseManager : DatabaseManagerProtocol {
     }
     
     @MainActor
-    static func makeShared(_ types: [any PersistentModel.Type]) -> DatabaseManager {
+    public static func makeShared(_ types: [any PersistentModel.Type]) -> DatabaseManager {
         assert(_shared == nil, "call only once")
         if _shared == nil {
             _shared = DatabaseManager(types)
@@ -25,7 +25,7 @@ public final class DatabaseManager : DatabaseManagerProtocol {
     private static var _shared: DatabaseManager!
     
     @MainActor
-    static var shared: DatabaseManager {
+    public static var shared: DatabaseManager {
         assert(_shared != nil, "please call makeShared first")
         return _shared!
     }
