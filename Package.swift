06d35607d82b843cc92f141981f1c444408c9e04
albert vila <albert.vila@inqbarna.com>
2025-07-07 08:38:54 +0200

    added spm

--- Package.swift
+++ Package.swift
@@ -9,11 +9,23 @@ let package = Package(
     products: [
         .library(name: "Alfy", targets: ["Alfy"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImageSVGCoder.git", from: "1.8.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "3.1.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.5"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.3.3"),
+    ],
     targets: [
         .target(name: "Alfy", path: "Sources"),
         .testTarget(
             name: "Tests",
-            dependencies: ["Alfy"],
+            dependencies: [
+                "Alfy",
+                "SDWebImageSVGCoder",
+                "SDWebImageSwiftUI",
+                "SwiftSoup",
+                "Kingfisher",
+            ],
             path: "Tests"
         )
     ]
